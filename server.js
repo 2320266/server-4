a73366b676bfa808395aadc0296f43e08bce9a04
232-266 <206805069+2320266@users.noreply.github.com>
2025-10-24 12:05:29 +0900

    js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from  'jsr:@hono/hono/deno';
+const app = new Hono();
+
+app.use(' /* ', serveStatic({root: './public' }));
+
+app.get(' api ', async(c) => {
+    const res = await fetch('https://randomuser.me/api/');
+    const data = await res.json();
+
+    console.log('AaBbCc');
+    return c.json({message: data});
+}) ;
+Deno.serve(app.fetch);
